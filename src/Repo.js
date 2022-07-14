73f2aca324ee74c20edb5c04920f93b65b6d30e1
Linh Hoang <lh@buildmidwestern.com>
2022-07-14 10:44:16 -0500

    removed console log

--- src/Repo.js
+++ src/Repo.js
@@ -11,7 +11,6 @@ export default function Repo() {
         const url = "https://api.github.com/repos/";
         axios.get(url + params.id + '/' + params.name).then((res) => {
             setInfo(res.data);
-            console.log(res)
         });
     }, []);
 
